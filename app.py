de6b11df758cba0edbef4b2f61fdc2fa22b52229
Eugene <gish.ee18@gmail.com>
2019-06-02 21:25:40 +0300

    Add license

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 """
-IOMirea-server - An updater for IOMirea messenger
+IOMirea-updater - An updater for IOMirea messenger
 Copyright (C) 2019  Eugene Ershov
 
 This program is free software: you can redistribute it and/or modify
